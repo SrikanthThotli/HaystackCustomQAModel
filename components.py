e3b2ef05a171ba299df50eb763f663f02895efb2
srikanth thotli <srikanthandreddy@gmail.com>
2022-10-12 11:21:36 +0530

    streamlit

--- components.py
+++ components.py
@@ -1,6 +1,7 @@
 import requests
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import pandas as pd
 import os
 from pathlib import Path
 import shutil
@@ -12,42 +13,13 @@ from haystack.pipelines import ExtractiveQAPipeline
 from haystack.nodes import FARMReader
 
 
-class BingSearch:
+class CustomSearch:
 
-    def __init__(self, subscription_key, url='https://api.bing.microsoft.com/v7.0/search'):
-        self.key = subscription_key
-        self.url = url
+    def __init__(self):
         self.reader_bert = FARMReader(model_name_or_path="distilbert-base-uncased-distilled-squad", use_gpu=True)
 
-    def bing_web_search(self, query):
-        # set parameters
-        search_url = self.url
-        headers = {"Ocp-Apim-Subscription-Key": self.key}
-        params = {"q": query, "textDecorations": True, "textFormat": "HTML"}
-        # get response
-        response = requests.get(search_url, headers=headers, params=params)
-        response.raise_for_status()
-        return response.json()
-
-    def webpageContentRetriever(self, url):
-        html = urlopen(url).read()
-        soup = BeautifulSoup(html, features="html.parser")
-        # kill all script and style elements
-        for script in soup(["script", "style"]):
-            script.extract()  # rip it out
-        # get text
-        text = soup.get_text()
-        # break into lines and remove leading and trailing space on each
-        lines = (line.strip() for line in text.splitlines())
-        # break multi-headlines into a line each
-        chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
-        # drop blank lines
-        text = '\n'.join(chunk for chunk in chunks if chunk)
-        return text
-
-    def updateBingFiles(self, search_query, doc_dir):
-        # doc_dir = "data/bingfiles/"
-        dirpath = Path('data') / 'bingfiles'
+    def updateDataFiles(self, doc_dir):
+        dirpath = Path('data') / 'files'
         if dirpath.exists() and dirpath.is_dir():
             shutil.rmtree(dirpath)
         try:
@@ -55,33 +27,34 @@ class BingSearch:
             print("Directory '%s' created successfully" % doc_dir)
         except OSError as error:
             print("Directory '%s' can not be created" % doc_dir)
-        # call API
-        search_results = self.bing_web_search(search_query)
-        size = len(search_results['webPages']['value'])
 
-        for ind in range(size):
-            try:
-                content = self.webpageContentRetriever(search_results['webPages']['value'][ind]['url'])
-            except:
-                continue
-            f = open(doc_dir + str(ind) + "_search_result.txt", "w+", encoding="utf-8")
-            f.write(content)
+        # Here we need to upload the data corpus of questions and answers which is present in csv file
+        # Here we have taken a limited set of questions and answers(~around 1000) to avoid memory issues in colab
+        data_file = pd.read_csv("data/CareerVillageDataSet.csv")
+        data_file['questions'] = data_file['questions'].fillna("No Question")
+        data_file['answers'] = data_file['answers'].fillna("No Answer")
+        data_file = data_file.astype(str)
+            
+        for ind in data_file.index:
+            doc_name = ''.join(e for e in data_file['questions'][ind] if e.isalnum())
+            f = open(doc_dir + str(ind) + "_" + doc_name + ".txt", "w+", encoding="utf-8")
+            f.write(data_file['answers'][ind])
             f.close()
 
     def getAnswer(self, search_query):
 
-        doc_dir = "data/bingfiles/"
-        self.updateBingFiles(search_query, doc_dir)
+        doc_dir = "data/files/"
+        self.updateDataFiles(doc_dir)
 
         # In-Memory Document Store
-        document_store_bing = InMemoryDocumentStore()
+        document_store_custom = InMemoryDocumentStore()
         docs = convert_files_to_docs(dir_path=doc_dir, clean_func=clean_wiki_text, split_paragraphs=True)
-        document_store_bing.write_documents(docs)
+        document_store_custom.write_documents(docs)
         # An in-memory TfidfRetriever based on Pandas dataframes
-        retriever_bing = TfidfRetriever(document_store=document_store_bing)
-        pipe_bing = ExtractiveQAPipeline(self.reader_bert, retriever_bing)
+        retriever_custom = TfidfRetriever(document_store=document_store_custom)
+        pipe_custom = ExtractiveQAPipeline(self.reader_bert, retriever_custom)
         print("***********************************" + search_query + "****************************************")
-        prediction = pipe_bing.run(query=search_query, params={"Retriever": {"top_k": 5}, "Reader": {"top_k": 1}})
+        prediction = pipe_custom.run(query=search_query, params={"Retriever": {"top_k": 5}, "Reader": {"top_k": 1}})
 
         result_answer = prediction['answers'][0]
         print("********************" + str(result_answer))
@@ -90,4 +63,4 @@ class BingSearch:
         actual_text = str(result_answer).split("context='")[1].split("',")[0]
 
         result_list = [{'answer': actual_answer, 'score': actual_score, 'text': actual_text}]
-        return result_list
+        return actual_answer
